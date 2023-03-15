0d316717f1f51ae1c7a774fbda9010122f646dfc
vaibhavdantakale1 <127470586+vaibhavdantakale1@users.noreply.github.com>
2023-03-15 13:32:20 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,25 @@
+class Substraction{
+
+public static void main(String []args){
+
+System.out.println("The two number are:");
+
+int a = 7;
+
+int b = 5;
+
+
+System.out.println(a + "" + b);
+
+
+
+
+int sub =a+b;
+
+
+
+
+System.out.println("The sub is = " + sub);
+
+
+}}
---
+++ Area.java
@@ -0,0 +1,9 @@
+class Area{
+public static void main(String []args){
+
+int width = 10;
+int height = 20;
+System.out.println(width +" "+height);
+int area =width*height;
+System.out.println("the aria is = "+ area);
+}}

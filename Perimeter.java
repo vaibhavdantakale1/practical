16e1d6a83c02c120ec21e095821d5e5b2c0d14f6
vaibhavdantakale1 <127470586+vaibhavdantakale1@users.noreply.github.com>
2023-03-16 10:48:06 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,11 @@
+public class Perimeter {
+static void rectanglePerimeter(int length, int width) {
+int perimeter = 2 * (length + width);
+                
+System.out.println("Rectangle Perimeter: " + perimeter);
+}	
+ 
+public static void main(String args[]){ 
+	
+rectanglePerimeter(20, 30); 
+}}
